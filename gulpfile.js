14a582f93624176455377072f08c7f3f19a34d30
aberkow <berkowitz.clarinet@gmail.com>
2016-04-02 22:38:52 -0400

    gulp tasks added and working

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
+var minifyHTML = require('gulp-minify-html');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+//report js errors
+gulp.task('jshint', function(){
+  return gulp.src('js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+//transpile scss to css
+gulp.task('sass', function(){
+  return gulp.src('scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'));
+});
+
+//watch for changes
+gulp.task('watch', function(){
+  gulp.watch('js/*.js', ['jshint']);
+  gulp.watch('scss/*.scss', ['sass']);
+});
+
+//default task
+gulp.task('default', ['jshint', 'sass', 'watch']);
+
+//minify index.html
+gulp.task('html', function(){
+  return gulp.src('index.html')
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('build/'));
+});
+
+//concat and minify js files
+gulp.task('scripts', function(){
+  return browserify('js/app.js')
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'));
+});
+
+//concat compiled css files
+gulp.task('styles', function(){
+  return gulp.src('css/*.css')
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('build/css'));
+});
+
+//image optimization
+gulp.task('images', function(){
+  return gulp.src('images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('build/images'));
+});
+
+//build task
+gulp.task('build', ['jshint', 'sass', 'html', 'scripts', 'styles', 'images']);
